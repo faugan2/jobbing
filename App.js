45a9739607db899d2b0788b198421d7eb98f6d9a
faugan <foganbidi2@gmail.com>
2022-01-31 23:27:02 +0000

    changes

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import  store  from './app/store'
+import { Provider } from 'react-redux'
+import {KEY} from "@env";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>{KEY}</Text>
+          <StatusBar style="auto" />
+        </View>
+    </Provider>
+    
   );
 }
 
---
+++ app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from '../features/appSlice'
+
+export default configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+})
